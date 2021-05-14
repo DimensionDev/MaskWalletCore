e05677a218c9158fd59036af4030978fc2ae95cf
jk234ert <handan0524@gmail.com>
2021-05-14 14:43:43 +0800

    chore: update comments

--- target-mobile/src/lib.rs
+++ target-mobile/src/lib.rs
@@ -8,8 +8,9 @@ pub struct RustByteSlice {
 
 /// # Safety
 ///
-/// The caller should provide a pointer that points to a valid C string with a NUL terminator of size less than `isize::MAX`
+/// The caller should provide a pointer that points to a valid bytes array of size eqqual to `len`
 #[no_mangle]
+#[cfg(target_os = "ios")]
 pub unsafe extern "C" fn rust_request(bytes: *const u8, len: usize) -> RustByteSlice {
     let byte_slice = slice::from_raw_parts(bytes, len as usize);
 
@@ -25,8 +26,9 @@ pub unsafe extern "C" fn rust_request(bytes: *const u8, len: usize) -> RustByteS
 
 /// # Safety
 ///
-/// The caller should provide a pointer that points to a valid C string with a NUL terminator of size less than `isize::MAX`.
+/// The caller should provide a `RustByteSlice` struct with a valid bytes array and its size
 #[no_mangle]
+#[cfg(target_os = "ios")]
 pub unsafe extern "C" fn rust_free(input: RustByteSlice) {
     let slice = slice::from_raw_parts_mut(input.bytes as *mut u8, input.len as usize);
     let _: Box<[u8]> = Box::from_raw(slice);
