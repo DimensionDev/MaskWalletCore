14fe7be9f83f2f8435051160947a449d7d59b34a
jk234ert <handan0524@gmail.com>
2021-06-08 20:56:52 +0800

    fix: rand not support on wasm

--- crypto/src/number_util.rs
+++ crypto/src/number_util.rs
@@ -1,7 +1,16 @@
-use rand::{thread_rng, RngCore};
 use std::vec;
 
+#[cfg(target_arch = "wasm32")]
 pub fn random_iv(len: usize) -> Vec<u8> {
+    use getrandom::getrandom;
+    let mut v = vec![0u8; len];
+    getrandom(&mut v);
+    v
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn random_iv(len: usize) -> Vec<u8> {
+    use rand::{thread_rng, RngCore};
     let mut v = vec![0u8; len];
     thread_rng().fill_bytes(&mut v);
     v
