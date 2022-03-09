912e99df9edb54d0d276950fc4cd653b499d1dd2
foxsin10 <yzjcnn@gmail.com>
2022-03-09 19:37:50 +0800

    feat: update xcframework script

--- target-mobile/build-xcframework.sh
+++ target-mobile/build-xcframework.sh
@@ -33,19 +33,22 @@ while [[ "$#" -gt 0 ]]; do case $1 in
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 TARGET_ROOT="$(cd ../target &>/dev/null && pwd)"
 CARGO="$HOME/.cargo/bin/cargo"
+LIBS_DIR=""
 
 cargo_build() {
     TARGET=$1
     LIBS_DIR="$TARGET_ROOT/$TARGET/$BUILD_PROFILE"
-    "$CARGO" "build" --target "$TARGET"
+
+    echo "--$TARGET-- $TARGET_ROOT"
+    "$CARGO" "build" --target "$TARGET" --release
 }
 
 # Intel iOS simulator
-# CFLAGS_x86_64_apple_ios="-target x86_64-apple-ios" \
-#     cargo_build x86_64-apple-ios
+CFLAGS_x86_64_apple_ios="-target x86_64-apple-ios" \
+    cargo_build x86_64-apple-ios
 
 # Hardware iOS targets
-# cargo_build aarch64-apple-ios
+cargo_build aarch64-apple-ios
 
 #
 # M1 iOS simulator.
@@ -73,24 +76,7 @@ mkdir -p "$COMMON/Modules"
 cp "$THIS_DIR/module.modulemap" "$COMMON/Modules/"
 
 mkdir -p "$COMMON/Headers"
-cp "$THIS_DIR/MozillaRustComponents.h" "$COMMON/Headers"
-cp "$REPO_ROOT/components/rc_log/ios/RustLogFFI.h" "$COMMON/Headers"
-cp "$REPO_ROOT/components/viaduct/ios/RustViaductFFI.h" "$COMMON/Headers"
-cp "$REPO_ROOT/components/external/glean/glean-core/ffi/glean.h" "$COMMON/Headers"
-# TODO: https://github.com/mozilla/uniffi-rs/issues/1060
-# it would be neat if there was a single UniFFI command that would spit out
-# all of the generated headers for all UniFFIed dependencies of a given crate.
-# For now we generate the Swift bindings to get the headers as a side effect,
-# then delete the generated Swift code. Bleh.
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/nimbus/src/nimbus.udl" -l swift -o "$COMMON/Headers"
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/crashtest/src/crashtest.udl" -l swift -o "$COMMON/Headers"
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/fxa-client/src/fxa_client.udl" -l swift -o "$COMMON/Headers"
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/logins/src/logins.udl" -l swift -o "$COMMON/Headers"
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/autofill/src/autofill.udl" -l swift -o "$COMMON/Headers"
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/push/src/push.udl" -l swift -o "$COMMON/Headers"
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/tabs/src/tabs.udl" -l swift -o "$COMMON/Headers"
-# $CARGO uniffi-bindgen generate "$REPO_ROOT/components/places/src/places.udl" -l swift -o "$COMMON/Headers"
-# rm -rf "$COMMON"/Headers/*.swift
+cp "$THIS_DIR/$FRAMEWORK_NAME.h" "$COMMON/Headers"
 
 # Flesh out the framework for each architecture based on the common files.
 # It's a little fiddly, because we apparently need to put all the simulator targets
@@ -101,20 +87,19 @@ cp "$REPO_ROOT/components/external/glean/glean-core/ffi/glean.h" "$COMMON/Header
 # iOS hardware
 mkdir -p "$XCFRAMEWORK_ROOT/ios-arm64"
 cp -r "$COMMON" "$XCFRAMEWORK_ROOT/ios-arm64/$FRAMEWORK_NAME.framework"
-cp "$TARGET_DIR/aarch64-apple-ios/$BUILD_PROFILE/$LIB_NAME" "$XCFRAMEWORK_ROOT/ios-arm64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME"
+cp "$TARGET_ROOT/aarch64-apple-ios/$BUILD_PROFILE/$LIB_NAME" "$XCFRAMEWORK_ROOT/ios-arm64/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME"
 
 # iOS simulator, with both platforms as a fat binary for mysterious reasons
 mkdir -p "$XCFRAMEWORK_ROOT/ios-arm64_x86_64-simulator"
 cp -r "$COMMON" "$XCFRAMEWORK_ROOT/ios-arm64_x86_64-simulator/$FRAMEWORK_NAME.framework"
 lipo -create \
   -output "$XCFRAMEWORK_ROOT/ios-arm64_x86_64-simulator/$FRAMEWORK_NAME.framework/$FRAMEWORK_NAME" \
-  "$TARGET_DIR/aarch64-apple-ios-sim/$BUILD_PROFILE/$LIB_NAME" \
-  "$TARGET_DIR/x86_64-apple-ios/$BUILD_PROFILE/$LIB_NAME"
+  "$TARGET_ROOT/aarch64-apple-ios-sim/$BUILD_PROFILE/$LIB_NAME" \
+  "$TARGET_ROOT/x86_64-apple-ios/$BUILD_PROFILE/$LIB_NAME" 
 
 # Set up the metadata for the XCFramework as a whole.
 
 cp "$THIS_DIR/Info.plist" "$XCFRAMEWORK_ROOT/Info.plist"
-cp "$THIS_DIR/DEPENDENCIES.md" "$XCFRAMEWORK_ROOT/DEPENDENCIES.md"
 
 rm -rf "$XCFRAMEWORK_ROOT/common"
 
--- target-mobile/build.rs
+++ target-mobile/build.rs
@@ -9,5 +9,5 @@ fn main() {
         .with_crate(crate_dir)
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("rustlib.h");
+        .write_to_file("libmask_wallet_core_mobile.h");
 }
